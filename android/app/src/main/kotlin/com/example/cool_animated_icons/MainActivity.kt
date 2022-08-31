a6f5f7c19f4790005f8ce057927d00355299e4ae
adil-ashfaq35 <adilashfaq500@gamil.com>
2022-08-31 10:40:46 +0500

    first commit

---
+++ android/app/src/main/kotlin/com/example/cool_animated_icons/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cool_animated_icons
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/view/myHomePage.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  @override
+  void initState() {
+      _animationController=AnimationController(vsync: this,duration: const Duration(seconds: 1));
+    super.initState();
+  }
+  bool currentPlaying=false;
+  void _iconTapped() {
+    if (currentPlaying == false) {
+      currentPlaying = true;
+      _animationController.forward();
+    } else {
+      currentPlaying = false;
+      _animationController.reverse();
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: GestureDetector(
+          onTap: _iconTapped,
+          child: AnimatedIcon(
+            icon: AnimatedIcons.play_pause,
+            progress: _animationController,
+            size: 200,
+          ),
+        ),
+      ),
+    );
+  }
+}
